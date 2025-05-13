496170b680ad76edec2cd98c6664d78fc9d1a9fa
Jonatan Gezelius <jonatan.gezelius@etteplan.com>
2025-05-13 08:46:58 +0000

    Add fpu operation and print float. Add hardware float support for devices with it.

---
+++ atsamc21xp-conf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+west build -p always -b samc21n_xpro/samc21n18a -t menuconfig
--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/random/random.h>
 #include "say_hello.h"
+#include <stdint.h>
 
 // Settings
 static const int32_t sleep_time_ms = 1000;
@@ -10,45 +11,49 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(my_led), gpios)
 
 int main(void)
 {
-  int ret;
-  int state = 0;
-  uint32_t rnd;
+    int ret;
+    int state = 0;
+    uint32_t rnd;
+    double dbl;
 
-  // Make sure that the GPIO was initialized
-  if (!gpio_is_ready_dt(&led))
-  {
-    return 0;
-  }
-
-  // Set the GPIO as output
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-  if (ret < 0)
-  {
-    return 0;
-  }
-
-  // Do forever
-  while (1)
-  {
-
-    // Change the state of the pin and print
-    state = !state;
-    rnd = sys_rand32_get();
-    printk("LED state: %d\r\n", state);
-    printk("Random value: %u\r\n", rnd);
-
-    say_hello();
-
-    // Set pin state
-    ret = gpio_pin_set_dt(&led, state);
-    if (ret < 0)
+    // Make sure that the GPIO was initialized
+    if (!gpio_is_ready_dt(&led))
     {
-      return 0;
+        return 0;
     }
 
-    // Sleep
-    k_msleep(sleep_time_ms);
-  }
+    // Set the GPIO as output
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+    if (ret < 0)
+    {
+        return 0;
+    }
 
-  return 0;
+    // Do forever
+    while (1)
+    {
+
+        // Change the state of the pin and print
+        state = !state;
+        rnd = sys_rand32_get();
+        dbl = rnd / UINT32_MAX;
+
+        printk("LED state: %d\r\n", state);
+        printk("Random value: %u\r\n", rnd);
+        printk("Random double value: %f\r\n", dbl);
+
+        say_hello();
+
+        // Set pin state
+        ret = gpio_pin_set_dt(&led, state);
+        if (ret < 0)
+        {
+            return 0;
+        }
+
+        // Sleep
+        k_msleep(sleep_time_ms);
+    }
+
+    return 0;
 }
